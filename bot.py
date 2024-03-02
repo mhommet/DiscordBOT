2cf9f3cb443f94c10c2442b0151a636275832b9a
nokk <milan.hommet@protonmail.com>
2024-03-02 14:25:57 +0100

    Deploy

--- bot.py
+++ bot.py
@@ -1,12 +1,10 @@
 import os
 import time
 from typing import Final
-
 import random2 as random
 from discord import Client, Intents
 from discord.ext import commands
 from dotenv import load_dotenv
-
 from music_cog import music_cog
 
 # Token

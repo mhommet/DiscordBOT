d2f23a89cc6a870bf80149b3801af1b9ab4c084a
nokk <milan.hommet@protonmail.com>
2024-03-02 18:47:46 +0100

    feat: allow users to add entire playlists to the queue

--- music_cog.py
+++ music_cog.py
@@ -9,6 +9,7 @@ class music_cog(commands.Cog):
 
         self.is_playing = False
         self.is_paused = False
+        self.is_skipping = False
 
         self.music_queue = []
         self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'False'}
@@ -21,11 +22,17 @@ class music_cog(commands.Cog):
             try:
                 info = ydl.extract_info(item, download=False)
                 if '_type' in info and info['_type'] == 'playlist':
-                    return [{'source': song['url'], 'title': song['title']} for song in info['entries']]
+                    songs = []
+                    for song in info['entries']:
+                        try:
+                            songs.append({'source': song['url'], 'title': song['title']})
+                        except Exception:
+                            continue
+                    return songs
                 else:
                     return {'source': info['url'], 'title': info['title']}
             except Exception:
-                return False
+                return None
 
     def play_next(self):
         if len(self.music_queue) > 0:
@@ -35,7 +42,12 @@ class music_cog(commands.Cog):
 
             self.music_queue.pop(0)
 
-            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+            if not self.is_skipping:
+                if not self.vc.is_playing():
+                    self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next() if not self.is_skipping else None)
+            else:
+                self.client.loop.create_task(self.wait_and_play_next())
+                
         else:
             self.is_playing = False
             self.client.loop.create_task(self.wait_and_disconnect())
@@ -45,8 +57,20 @@ class music_cog(commands.Cog):
         if not self.is_playing:
             await self.vc.disconnect()
             self.vc = None
+            
+    async def wait_and_play_next(self):
+        await asyncio.sleep(1)
+        self.is_skipping = False
+        self.play_next()
 
     async def play_music(self, ctx):
+        try:
+            if self.vc is None or not self.vc.is_connected():
+                self.vc = await self.music_queue[0][1].channel.connect()
+        except Exception as e:
+            print(f"An error occurred while connecting to the voice channel: {e}")
+            return
+            
         if len(self.music_queue) > 0:
             self.is_playing = True
             m_url = self.music_queue[0][0]['source']
@@ -59,7 +83,7 @@ class music_cog(commands.Cog):
                     return
 
             else:
-                await self.vc.move_to(self.music_queue[0][1])
+                await self.vc.move_to(self.music_queue[0][1].channel)
 
             self.music_queue.pop(0)
 
@@ -78,16 +102,17 @@ class music_cog(commands.Cog):
             self.vs.resume()
         else:
             songs = self.search_yt(query)
-            if songs is False:
-                await ctx.send("Je n'ai pas pu trouver la musique. Assurez-vous que l'url de la musique est correcte.")
+            if songs is None:
+                await ctx.send("La musique n'est pas disponible.")
             else:
                 if isinstance(songs, list):
                     for song in songs:
                         if isinstance(song, dict):
                             self.music_queue.append([song, ctx.author.voice])
-                elif isinstance(songs, dict): 
-                        self.music_queue.append([song, ctx.author.voice])
-                await ctx.send("Musique ajoutée à la file d'attente")
+                    await ctx.send("Musiques ajoutées à la file d'attente")
+                elif isinstance(songs, dict):
+                    self.music_queue.append([songs, ctx.author.voice])
+                    await ctx.send("Musique ajoutée à la file d'attente")
                 if self.is_playing == False:
                     await self.play_music(ctx)
 
@@ -111,19 +136,22 @@ class music_cog(commands.Cog):
 
     @commands.command(name="skip", aliases=["s"], description="Passe à la musique suivante")
     async def skip(self, ctx, *args):
-        if self.vc != None and self.vc:
+        if self.vc != None and self.vc.is_playing():
+            self.is_skipping = True
             self.vc.stop()
-            await self.play_music(ctx)
+            self.client.loop.create_task(self.wait_and_play_next())
+        else:
+            await ctx.send("Je ne suis pas en train de jouer de la musique.")
 
     @commands.command(name="queue", aliases=["q"], description="Montre la file d'attente")
     async def queue(self, ctx):
-        retval = ""
+        if len(self.music_queue) > 0:
+            retval = "```\n"
 
-        for i in range(0, len(self.music_queue)):
-            if i > 4: break
-            retval += self.music_queue[i][0]['title'] + "\n"
+            for i in range(len(self.music_queue)):
+                retval += f"- {self.music_queue[i][0]['title']}\n"
 
-        if retval != "":
+            retval += "```"
             await ctx.send(retval)
         else:
             await ctx.send("La file d'attente est vide.")
